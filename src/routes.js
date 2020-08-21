ed9a8c1fc3414b60ae60cf2407f4968197cd54d0
Felipe Joazeiro <felipejoazeiro@gmail.com>
2020-08-21 19:12:15 -0300

    Fim do modulo

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,10 @@ routes.get('/', (req,res)=>{
 })
 routes.get('/products/create', ProductController.create)
 routes.post('/products', ProductController.post)
+routes.get('/products/:id/edit', ProductController.edit)
+routes.put('/products', ProductController.put)
+routes.delete('/products', ProductController.delete)
+
 
 //Alias
 routes.get('/ads/create', (req,res)=>{
